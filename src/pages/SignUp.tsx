b0cf069f9c76c7968752db4aa7d8310182b7e90d
cung nguyen <cung.fpt@carehealth.io>
2024-10-16 22:58:04 +0700

    Add google authentication

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -2,6 +2,7 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({
@@ -47,6 +48,7 @@ export default function SignUp() {
       setLoading(false);
     }
   };
+
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -109,6 +111,7 @@ export default function SignUp() {
                 "Sign Up"
               )}
             </Button>
+            <OAuth />
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Have an account?</span>
