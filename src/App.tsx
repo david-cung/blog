7548a6164698f3df5846f29fc5bfb232b9f64c56
cung nguyen <cung.fpt@carehealth.io>
2024-11-11 21:05:58 +0700

    Add create post UI

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Projects from "./pages/Projects";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
+import CreatePost from "./pages/CreatePost";
 
 export default function App() {
   return (
@@ -20,6 +21,7 @@ export default function App() {
         <Route path='/sign-up' element={<SignUp />} />
         <Route element={<PrivateRoute />}>
           <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/create-post' element={<CreatePost />} />
         </Route>
         <Route path='/projects' element={<Projects />} />
       </Routes>
--- src/components/DashProfile.tsx
+++ src/components/DashProfile.tsx
@@ -11,16 +11,27 @@ import {
 import { app } from "../firebase";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import {
+  updateStart,
+  updateSuccess,
+  updateFailure,
+} from "../redux/user/userSlice";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
 export default function DashProfile() {
-  const { currentUser } = useSelector((state: any) => state.user);
+  const { currentUser, loading } = useSelector((state: any) => state.user);
   const [imageFile, setImageFile] = useState<any>(null);
   const [imageFileUrl, setImageFileUrl] = useState<string | null>(null);
   const [imageFileUploadProgress, setImageFileUploadProgress] =
     useState<any>(null);
+  const [updateUserError, setUpdateUserError] = useState<any>("");
   const [imageFileUploadError, setImageFileUploadError] = useState<any>(null);
-  console.log(imageFileUploadProgress, imageFileUploadError);
+  const [imageFileUploading, setImageFileUploading] = useState<boolean>(false);
+  const [updateUserSuccess, setUpdateUserSuccess] = useState<string>("");
+  const [formData, setFormData] = useState<any>({});
   const filePickerRef = useRef<HTMLInputElement | null>(null);
+  const dispatch = useDispatch();
 
   const handleImageChange = (e: any) => {
     const file = e.target.files[0];
@@ -36,6 +47,7 @@ export default function DashProfile() {
   }, [imageFile]);
 
   const uploadImage = async () => {
+    setImageFileUploading(true);
     setImageFileUploadProgress(null);
     const storage = getStorage(app);
     const fileName = new Date().getTime() + imageFile?.name;
@@ -48,6 +60,7 @@ export default function DashProfile() {
           (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         setImageFileUploadProgress(progress.toFixed(0));
       },
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       (error) => {
         setImageFileUploadError(
           "Could not upload image (File must be less than 2MB"
@@ -55,19 +68,57 @@ export default function DashProfile() {
         setImageFileUploadProgress(null);
         setImageFile(null);
         setImageFileUrl(null);
+        setImageFileUploading(false);
       },
       () => {
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
           setImageFileUrl(downloadURL);
+          setFormData({ ...formData, photoURL: downloadURL });
+          setImageFileUploading(false);
         });
       }
     );
+  };
+  const handleChange = (e: any) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setUpdateUserError(null);
+    setUpdateUserSuccess("");
+    if (Object.keys(formData).length === 0) {
+      setUpdateUserError("No changes made to the profile");
+      return;
+    }
+    if (imageFileUploading) {
+      setUpdateUserError("Please wait while the image is being uploaded");
+      return;
+    }
+    try {
+      dispatch(updateStart());
+      const res = await fetch(`/v1/users/${currentUser.data.id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        dispatch(updateFailure(data.message));
+        setUpdateUserError(data.message);
+      } else {
+        dispatch(updateSuccess(data));
+        setUpdateUserSuccess("User's profile updated successfully");
+      }
+    } catch (error: any) {
+      dispatch(updateFailure(error.message));
+    }
   };
 
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
-      <form className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           type='file'
           accept='image/*'
@@ -116,20 +167,51 @@ export default function DashProfile() {
 
         <TextInput
           type='text'
-          id='username'
+          id='userName'
           placeholder='username'
           defaultValue={currentUser.data.userName}
+          onChange={handleChange}
         />
         <TextInput
           type='email'
           id='email'
           placeholder='email'
           defaultValue={currentUser.data.email}
+          onChange={handleChange}
         />
-        <TextInput type='password' id='password' placeholder='username' />
-        <Button type='submit' gradientDuoTone='purpleToBlue' outline>
-          Update
+        <TextInput
+          type='password'
+          id='password'
+          placeholder='password'
+          onChange={handleChange}
+        />
+        <Button
+          type='submit'
+          gradientDuoTone='purpleToBlue'
+          outline
+          disabled={loading || imageFileUploading}
+        >
+          {loading ? "Loading..." : "Update Profile"}
         </Button>
+        {updateUserSuccess && (
+          <Alert color='success' className='mt-5'>
+            {updateUserSuccess}
+          </Alert>
+        )}
+        {updateUserSuccess && (
+          <Alert color='failure' className='mt-5'>
+            {updateUserError}
+          </Alert>
+        )}
+        <Link to={"/create-post"}>
+          <Button
+            type='button'
+            gradientDuoTone='purpleToPink'
+            className='w-full'
+          >
+            Create a post
+          </Button>
+        </Link>
       </form>
       <div className='text-red-500 flex justify-between mt-5'>
         <span className='cursor-pointer'>Delete Account</span>
